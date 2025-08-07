8f0379a17b616d885fb1ec281b6f77172725b160
imeline <iseonju745@gmail.com>
2025-08-07 16:21:10 +0900

    feat: MyBatisConfig.transactionManager() 추가 - @transactional을 사용하기 위함

---
+++ src/main/java/erp/global/config/MyBatisConfig.java
@@ -0,0 +1,20 @@
+package erp.global.config;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.transaction.PlatformTransactionManager;
+
+import javax.sql.DataSource;
+
+@Configuration
+@MapperScan("erp")
+public class MyBatisConfig {
+
+    @Bean
+    // @transactional 사용 설정
+    public PlatformTransactionManager transactionManager(DataSource dataSource) {
+        return new DataSourceTransactionManager(dataSource);
+    }
+}
