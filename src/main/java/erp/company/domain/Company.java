808d466e7f48051fc95d7fce0ce9b2e30276464a
imeline <iseonju745@gmail.com>
2025-08-20 16:36:45 +0900

    feat: tenantId DB 컬럼으로 넣지 않고 자바 코드에서 명시적으로 사용하는걸로 변경

--- src/main/java/erp/company/domain/Company.java
+++ src/main/java/erp/company/domain/Company.java
@@ -13,4 +13,25 @@ public class Company extends TimeStamped {
     private String bizNo;
     private String address;
     private String phone;
+
+    public static Company register(String name, String bizNo, String address,
+                                   String phone) {
+        return Company.builder()
+                .name(name)
+                .bizNo(bizNo)
+                .address(address)
+                .phone(phone)
+                .build();
+    }
+
+    public static Company modify(long companyId, String name, String bizNo,
+                                 String address, String phone) {
+        return Company.builder()
+                .companyId(companyId)
+                .name(name)
+                .bizNo(bizNo)
+                .address(address)
+                .phone(phone)
+                .build();
+    }
 }
