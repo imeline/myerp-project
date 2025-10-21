cacc7b460f324f13f09f22e70106b5e71d821478
LEE SUNJU <128706341+imeline@users.noreply.github.com>
2025-10-21 19:39:04 +0900

    remove: client 코드 제거

--- client/tailwind.config.js
+++
@@ -1,7 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-export default {
-  content: ['./index.html', './src/**/*.{js,jsx}'],
-  darkMode: 'class', // ← 다크모드: .dark 클래스로 제어
-  theme: { extend: {} },
-  plugins: [],
-}
